a969a39ff3b0fa25bb0e6cd65ca64e1233df2853
choi mira <sky10702@naver.com>
2025-01-02 16:42:03 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,22 @@
 $(function () {
+
     $('.main_container').fullpage({
+        anchors: ['intro', 'service', 'center', 'news', 'video', 'customer'],
+        scrollOverflow: false,
         afterLoad: function (o, ds) {
             let idx = ds.index;
 
-            if (idx == 1) {
+            $('.section').removeClass('on');
+            $('.section').eq(idx).addClass('on');
+
+
+            $('.menu_lnk ul li').removeClass('on');
+            $('.menu_lnk ul li').eq(idx).addClass('on');
+
+            if (idx == 2) {
                 $('.header').addClass('on')
             } else {
-                $('header').removeClass('on')
+                $('.header').removeClass('on')
             }
         }
     });
